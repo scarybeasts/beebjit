34c1d0afd4b57a3a57a0020a450be51ce5908131
Chris Evans <scarybeasts@gmail.com>
2019-05-11 23:27:18 -0700

    Start to split the optimizer into separate passes for clarity.

--- jit_optimizer.c
+++ jit_optimizer.c
@@ -54,11 +54,11 @@ jit_optimizer_optimize(struct jit_compiler* p_compiler,
                        uint32_t num_opcodes) {
   uint32_t i_opcodes;
 
-  int32_t reg_a = k_value_unknown;
-  int32_t reg_x = k_value_unknown;
-  int32_t reg_y = k_value_unknown;
-  int32_t flag_carry = k_value_unknown;
-  int32_t flag_decimal = k_value_unknown;
+  int32_t reg_a;
+  int32_t reg_x;
+  int32_t reg_y;
+  int32_t flag_carry;
+  int32_t flag_decimal;
 
   struct jit_opcode_details* p_prev_opcode = NULL;
 
@@ -74,9 +74,19 @@ jit_optimizer_optimize(struct jit_compiler* p_compiler,
   (void) p_compiler;
   (void) flag_decimal;
 
+  /* Pass 1: tag opcodes with any known register and flag values. Also replace
+   * single uops with replacements if known registers offer better alternatives.
+   * Classic example is CLC; ADC. At the ADC instruction, it is known that
+   * CF==0 so the ADC can become just an ADD.
+   */
+  reg_a = k_value_unknown;
+  reg_x = k_value_unknown;
+  reg_y = k_value_unknown;
+  flag_carry = k_value_unknown;
+  flag_decimal = k_value_unknown;
   for (i_opcodes = 0; i_opcodes < num_opcodes; ++i_opcodes) {
-    uint32_t i_uops;
     uint32_t num_uops;
+    uint32_t i_uops;
     uint8_t opreg;
 
     struct jit_opcode_details* p_opcode = &p_opcodes[i_opcodes];
@@ -84,25 +94,156 @@ jit_optimizer_optimize(struct jit_compiler* p_compiler,
     uint16_t operand_6502 = p_opcode->operand_6502;
     uint8_t optype = g_optypes[opcode_6502];
     uint8_t opmode = g_opmodes[opcode_6502];
-    uint8_t opbranch = g_opbranch[optype];
-    int changes_nz = g_optype_changes_nz_flags[optype];
     int changes_carry = g_optype_changes_carry[optype];
+
+    p_opcode->num_fixup_uops = 0;
+    p_opcode->len_bytes_6502_merged = p_opcode->len_bytes_6502_orig;
+    p_opcode->max_cycles_merged = p_opcode->max_cycles_orig;
+    p_opcode->eliminated = 0;
 
     p_opcode->reg_a = reg_a;
     p_opcode->reg_x = reg_x;
     p_opcode->reg_y = reg_y;
     p_opcode->flag_carry = flag_carry;
     p_opcode->flag_decimal = flag_carry;
-    p_opcode->num_fixup_uops = 0;
-    p_opcode->len_bytes_6502_merged = p_opcode->len_bytes_6502_orig;
-    p_opcode->max_cycles_merged = p_opcode->max_cycles_orig;
-    p_opcode->eliminated = 0;
 
     /* TODO: seems hacky, should g_optype_sets_register just be per-opcode? */
     opreg = g_optype_sets_register[optype];
     if (opmode == k_acc) {
       opreg = k_a;
     }
+
+    num_uops = p_opcode->num_uops;
+    for (i_uops = 0; i_uops < num_uops; ++i_uops) {
+      struct jit_uop* p_uop = &p_opcode->uops[i_uops];
+      int32_t uopcode = p_uop->uopcode;
+
+      p_uop->eliminated = 0;
+
+      switch (uopcode) {
+      case 0x61: /* ADC idx */
+      case 0x75: /* ADC zpx */
+        if (flag_carry == 0) {
+          p_uop->uopcode = k_opcode_ADD_scratch;
+        }
+        break;
+      case 0x65: /* ADC zpg */
+      case 0x6D: /* ADC abs */
+        if (flag_carry == 0) {
+          p_uop->uopcode = k_opcode_ADD_ABS;
+        }
+        break;
+      case 0x69: /* ADC imm */
+        if (flag_carry == 0) {
+          p_uop->uopcode = k_opcode_ADD_IMM;
+        } else if (flag_carry == 1) {
+          /* NOTE: if this is common, we can optimize this case. */
+          printf("LOG:JIT:optimizer sees ADC #$imm with C==1\n");
+        }
+        break;
+      case 0x71: /* ADC idy */
+        if (flag_carry == 0) {
+          p_uop->uopcode = k_opcode_ADD_scratch_Y;
+        }
+        break;
+      case 0x79: /* ADC aby */
+        if (flag_carry == 0) {
+          p_uop->uopcode = k_opcode_ADD_ABY;
+        }
+        break;
+      case 0x7D: /* ADC abx */
+        if (flag_carry == 0) {
+          p_uop->uopcode = k_opcode_ADD_ABX;
+        }
+        break;
+      case 0x84: /* STY zpg */
+      case 0x8C: /* STY abs */
+        if (reg_y != k_value_unknown) {
+          p_uop->uopcode = k_opcode_STOA_IMM;
+          p_uop->value2 = reg_y;
+        }
+        break;
+      case 0x85: /* STA zpg */
+      case 0x8D: /* STA abs */
+        if (reg_a != k_value_unknown) {
+          p_uop->uopcode = k_opcode_STOA_IMM;
+          p_uop->value2 = reg_a;
+        }
+        break;
+      case 0x86: /* STX zpg */
+      case 0x8E: /* STX abs */
+        if (reg_x != k_value_unknown) {
+          p_uop->uopcode = k_opcode_STOA_IMM;
+          p_uop->value2 = reg_x;
+        }
+        break;
+      case 0xE9: /* SBC imm */
+        if (flag_carry == 1) {
+          p_uop->uopcode = k_opcode_SUB_IMM;
+        }
+        break;
+      default:
+        break;
+      }
+    }
+
+    /* Update known state of registers, flags, etc. for next opcode. */
+    switch (opreg) {
+    case k_a:
+      reg_a = k_value_unknown;
+      break;
+    case k_x:
+      reg_x = k_value_unknown;
+      break;
+    case k_y:
+      reg_y = k_value_unknown;
+      break;
+    default:
+      break;
+    }
+
+    if (changes_carry) {
+      flag_carry = k_value_unknown;
+    }
+
+    switch (opcode_6502) {
+    case 0x18: /* CLC */
+      flag_carry = 0;
+      break;
+    case 0x38: /* SEC */
+      flag_carry = 1;
+      break;
+    case 0xA0: /* LDY imm */
+      reg_y = operand_6502;
+      break;
+    case 0xA2: /* LDX imm */
+      reg_x = operand_6502;
+      break;
+    case 0xA9: /* LDA imm */
+      reg_a = operand_6502;
+      break;
+    case 0xD8: /* CLD */
+      flag_decimal = 0;
+      break;
+    case 0xF8: /* SED */
+      flag_decimal = 1;
+      break;
+    default:
+      break;
+    }
+  }
+
+  /* Pass 2: eliminate and merge opcodes as we can. */
+  for (i_opcodes = 0; i_opcodes < num_opcodes; ++i_opcodes) {
+    uint32_t i_uops;
+    uint32_t num_uops;
+
+    struct jit_opcode_details* p_opcode = &p_opcodes[i_opcodes];
+    uint8_t opcode_6502 = p_opcode->opcode_6502;
+    uint8_t optype = g_optypes[opcode_6502];
+    uint8_t opmode = g_opmodes[opcode_6502];
+    uint8_t opbranch = g_opbranch[optype];
+    int changes_nz = g_optype_changes_nz_flags[optype];
 
     /* Merge opcode into previous if supported. */
     if ((p_prev_opcode != NULL) &&
@@ -227,8 +368,6 @@ jit_optimizer_optimize(struct jit_compiler* p_compiler,
       struct jit_uop* p_uop = &p_opcode->uops[i_uops];
       int32_t uopcode = p_uop->uopcode;
 
-      p_uop->eliminated = 0;
-
       /* Keep track of uops we may be able to eliminate. */
       switch (uopcode) {
       case k_opcode_FLAGA:
@@ -255,120 +394,6 @@ jit_optimizer_optimize(struct jit_compiler* p_compiler,
       default:
         break;
       }
-
-      /* Optimize uopcodes that can be directly replaced based on knowledge of
-       * current register / flag values.
-       */
-      switch (uopcode) {
-      case 0x61: /* ADC idx */
-      case 0x75: /* ADC zpx */
-        if (flag_carry == 0) {
-          p_uop->uopcode = k_opcode_ADD_scratch;
-        }
-        break;
-      case 0x65: /* ADC zpg */
-      case 0x6D: /* ADC abs */
-        if (flag_carry == 0) {
-          p_uop->uopcode = k_opcode_ADD_ABS;
-        }
-        break;
-      case 0x69: /* ADC imm */
-        if (flag_carry == 0) {
-          p_uop->uopcode = k_opcode_ADD_IMM;
-        } else if (flag_carry == 1) {
-          /* NOTE: if this is common, we can optimize this case. */
-          printf("LOG:JIT:optimizer sees ADC #$imm with C==1\n");
-        }
-        break;
-      case 0x71: /* ADC idy */
-        if (flag_carry == 0) {
-          p_uop->uopcode = k_opcode_ADD_scratch_Y;
-        }
-        break;
-      case 0x79: /* ADC aby */
-        if (flag_carry == 0) {
-          p_uop->uopcode = k_opcode_ADD_ABY;
-        }
-        break;
-      case 0x7D: /* ADC abx */
-        if (flag_carry == 0) {
-          p_uop->uopcode = k_opcode_ADD_ABX;
-        }
-        break;
-      case 0x84: /* STY zpg */
-      case 0x8C: /* STY abs */
-        if (reg_y != k_value_unknown) {
-          p_uop->uopcode = k_opcode_STOA_IMM;
-          p_uop->value2 = reg_y;
-        }
-        break;
-      case 0x85: /* STA zpg */
-      case 0x8D: /* STA abs */
-        if (reg_a != k_value_unknown) {
-          p_uop->uopcode = k_opcode_STOA_IMM;
-          p_uop->value2 = reg_a;
-        }
-        break;
-      case 0x86: /* STX zpg */
-      case 0x8E: /* STX abs */
-        if (reg_x != k_value_unknown) {
-          p_uop->uopcode = k_opcode_STOA_IMM;
-          p_uop->value2 = reg_x;
-        }
-        break;
-      case 0xE9: /* SBC imm */
-        if (flag_carry == 1) {
-          p_uop->uopcode = k_opcode_SUB_IMM;
-        }
-        break;
-      default:
-        break;
-      }
-    }
-
-    /* Update known state of registers, flags, etc. for next opcode. */
-    switch (opreg) {
-    case k_a:
-      reg_a = k_value_unknown;
-      break;
-    case k_x:
-      reg_x = k_value_unknown;
-      break;
-    case k_y:
-      reg_y = k_value_unknown;
-      break;
-    default:
-      break;
-    }
-
-    if (changes_carry) {
-      flag_carry = k_value_unknown;
-    }
-
-    switch (opcode_6502) {
-    case 0x18: /* CLC */
-      flag_carry = 0;
-      break;
-    case 0x38: /* SEC */
-      flag_carry = 1;
-      break;
-    case 0xA0: /* LDY imm */
-      reg_y = operand_6502;
-      break;
-    case 0xA2: /* LDX imm */
-      reg_x = operand_6502;
-      break;
-    case 0xA9: /* LDA imm */
-      reg_a = operand_6502;
-      break;
-    case 0xD8: /* CLD */
-      flag_decimal = 0;
-      break;
-    case 0xF8: /* SED */
-      flag_decimal = 1;
-      break;
-    default:
-      break;
     }
 
     p_prev_opcode = p_opcode;
