802927d94b4d491248186ec64f6943d2b57f1e22
Chris Evans <scarybeasts@gmail.com>
2019-07-25 23:04:40 -0700

    Rename executable from 6502jit to beebjit.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-gcc -O3 -DNDEBUG -Wall -W -Werror -g -o 6502jit \
+gcc -O3 -DNDEBUG -Wall -W -Werror -g -o beebjit \
     main.c bbc.c jit.c defs_6502.c debug.c util.c state.c video.c via.c \
     emit_6502.c interp.c inturbo.c state_6502.c sound.c intel_fdc.c timing.c \
     jit_compiler.c cpu_driver.c asm_x64_abi.c asm_tables.c \
@@ -10,7 +10,7 @@ gcc -O3 -DNDEBUG -Wall -W -Werror -g -o 6502jit \
     jit_optimizer.c jit_opcode.c keyboard.c os.c \
     -lm -lX11 -lXext -lpthread -lasound
 
-gcc -Wall -W -Werror -g -o 6502jit \
+gcc -Wall -W -Werror -g -o beebjit \
     main.c bbc.c jit.c defs_6502.c debug.c util.c state.c video.c via.c \
     emit_6502.c interp.c inturbo.c state_6502.c sound.c intel_fdc.c timing.c \
     jit_compiler.c cpu_driver.c asm_x64_abi.c asm_tables.c \
@@ -29,35 +29,35 @@ gcc -Wall -W -Werror -g -o make_perf_rom make_perf_rom.c \
 ./make_timing_rom
 
 #echo 'Running built-in tests.'
-#./6502jit -t
+#./beebjit -t
 echo 'Running test.rom, JIT, fast.'
-./6502jit -os test.rom -expect 434241 -mode jit -f
+./beebjit -os test.rom -expect 434241 -mode jit -f
 echo 'Running test.rom, JIT, fast, debug.'
-./6502jit -os test.rom -expect 434241 -mode jit -f -d -r
+./beebjit -os test.rom -expect 434241 -mode jit -f -d -r
 echo 'Running test.rom, JIT, fast, accurate.'
-./6502jit -os test.rom -expect 434241 -mode jit -f -a
+./beebjit -os test.rom -expect 434241 -mode jit -f -a
 echo 'Running test.rom, interpreter, fast.'
-./6502jit -os test.rom -expect 434241 -mode interp -f
+./beebjit -os test.rom -expect 434241 -mode interp -f
 echo 'Running test.rom, interpreter, fast, debug, print.'
-./6502jit -os test.rom -expect 434241 -mode interp -f -d -r -p >/dev/null
+./beebjit -os test.rom -expect 434241 -mode interp -f -d -r -p >/dev/null
 echo 'Running test.rom, interpreter, slow.'
-./6502jit -os test.rom -expect 434241 -mode interp
+./beebjit -os test.rom -expect 434241 -mode interp
 echo 'Running test.rom, inturbo, fast.'
-./6502jit -os test.rom -expect 434241 -mode inturbo -f
+./beebjit -os test.rom -expect 434241 -mode inturbo -f
 echo 'Running test.rom, inturbo, fast, debug.'
-./6502jit -os test.rom -expect 434241 -mode inturbo -f -d -r
+./beebjit -os test.rom -expect 434241 -mode inturbo -f -d -r
 echo 'Running test.rom, inturbo, fast, accurate.'
-./6502jit -os test.rom -expect 434241 -mode inturbo -f -a
+./beebjit -os test.rom -expect 434241 -mode inturbo -f -a
 
 echo 'Running timing.rom, interpreter, slow.'
-./6502jit -os timing.rom -expect C0C1C2 -mode interp
+./beebjit -os timing.rom -expect C0C1C2 -mode interp
 echo 'Running timing.rom, interpreter, fast.'
-./6502jit -os timing.rom -expect C0C1C2 -mode interp -f -a
+./beebjit -os timing.rom -expect C0C1C2 -mode interp -f -a
 echo 'Running timing.rom, inturbo, fast.'
-./6502jit -os timing.rom -expect C0C1C2 -mode inturbo -f -a
+./beebjit -os timing.rom -expect C0C1C2 -mode inturbo -f -a
 echo 'Running timing.rom, jit, fast.'
-./6502jit -os timing.rom -expect C0C1C2 -mode jit -f -a
+./beebjit -os timing.rom -expect C0C1C2 -mode jit -f -a
 echo 'Running timing.rom, jit, fast, debug.'
-./6502jit -os timing.rom -expect C0C1C2 -mode jit -f -a -d -r
+./beebjit -os timing.rom -expect C0C1C2 -mode jit -f -a -d -r
 
 echo 'All is well!'
