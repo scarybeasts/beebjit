b27b3eed06e7f63c76f6bf62c5089050477aafff
Chris Evans <scarybeasts@gmail.com>
2019-11-05 11:52:53 -0800

    Improve teletext rendering, making most screens recognizable.
    - Foreground and background colors.
    - Selection between glphys and graphics.

--- teletext.c
+++ teletext.c
@@ -18,11 +18,19 @@
 #include <string.h>
 
 struct teletext_struct {
+  uint32_t palette[8];
   uint32_t scanline;
+  uint8_t* p_active_characters;
+  int graphics_active;
+  int separated_active;
+  uint32_t fg_color;
+  uint32_t bg_color;
 };
 
 struct teletext_struct*
 teletext_create() {
+  uint32_t i;
+
   struct teletext_struct* p_teletext = malloc(sizeof(struct teletext_struct));
   if (p_teletext == NULL) {
     errx(1, "cannot allocate teletext_struct");
@@ -30,9 +38,20 @@ teletext_create() {
 
   (void) memset(p_teletext, '\0', sizeof(struct teletext_struct));
 
-  (void) teletext_characters;
-  (void) teletext_graphics;
-  (void) teletext_separated_graphics;
+  for (i = 0; i < 8; ++i) {
+    /* Black, full alpha. */
+    uint32_t color = 0xff000000;
+    if (i & 1) {
+      color |= 0xff0000;
+    }
+    if (i & 2) {
+      color |= 0xff00;
+    }
+    if (i & 4) {
+      color |= 0xff;
+    }
+    p_teletext->palette[i] = color;
+  }
 
   return p_teletext;
 }
@@ -43,17 +62,95 @@ teletext_destroy(struct teletext_struct* p_teletext) {
 }
 
 static void
-teletext_render_line(uint8_t* p_src_chars,
+teletext_set_active_characters(struct teletext_struct* p_teletext) {
+  if (p_teletext->graphics_active) {
+    if (p_teletext->separated_active) {
+      p_teletext->p_active_characters = &teletext_separated_graphics[0];
+    } else {
+      p_teletext->p_active_characters = &teletext_graphics[0];
+    }
+  } else {
+    p_teletext->p_active_characters = &teletext_characters[0];
+  }
+}
+
+void
+teletext_scanline_ended(struct teletext_struct* p_teletext) {
+  p_teletext->graphics_active = 0;
+  p_teletext->separated_active = 0;
+  p_teletext->fg_color = p_teletext->palette[7];
+  p_teletext->bg_color = p_teletext->palette[0];
+
+  teletext_set_active_characters(p_teletext);
+}
+
+static void
+teletext_handle_control_character(struct teletext_struct* p_teletext,
+                                  uint8_t src_char) {
+  switch (src_char) {
+  case 0:
+    /* NOTE: SAA5050 appears to be a pre-2.5 presentation level, which doesn't
+     * have the ability to select black.
+     * See: https://www.etsi.org/deliver/etsi_i_ets/300700_300799/300706/01_60/ets_300706e01p.pdf
+     */
+    break;
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+  case 7:
+    p_teletext->graphics_active = 0;
+    p_teletext->fg_color = p_teletext->palette[src_char];
+    break;
+  case 16:
+    /* Can't select black graphics -- see above. */
+    break;
+  case 17:
+  case 18:
+  case 19:
+  case 20:
+  case 21:
+  case 22:
+  case 23:
+    p_teletext->graphics_active = 1;
+    p_teletext->fg_color = p_teletext->palette[(src_char & 7)];
+    break;
+  case 25:
+    p_teletext->separated_active = 0;
+    break;
+  case 26:
+    p_teletext->separated_active = 1;
+    break;
+  case 28:
+    p_teletext->bg_color = p_teletext->palette[0];
+    break;
+  case 29:
+    p_teletext->bg_color = p_teletext->fg_color;
+    break;
+  }
+
+  teletext_set_active_characters(p_teletext);
+}
+
+static void
+teletext_render_line(struct teletext_struct* p_teletext,
+                     uint8_t* p_src_chars,
                      uint32_t scanline,
                      uint32_t* p_dest_buffer) {
   uint32_t column;
   uintptr_t src_chars = (uintptr_t) p_src_chars;
 
+  teletext_scanline_ended(p_teletext);
+
   for (column = 0; column < 40; ++column) {
     uint8_t src_char;
     uint32_t value;
-    uint32_t alpha = 0xff000000;
-    uint8_t* p_src_data = &teletext_characters[0];
+    uint32_t bg_color;
+    uint32_t fg_color;
+    /* Selects space, 0x20. */
+    uint8_t* p_src_data = p_teletext->p_active_characters;
 
     /* TODO: can abstract this. */
     if (src_chars & 0x8000) {
@@ -63,34 +160,38 @@ teletext_render_line(uint8_t* p_src_chars,
     src_char = ((*((uint8_t*) src_chars)) & 0x7F);
     src_chars++;
 
-    /* Point at space (0x20). */
-    if (src_char > 0x20) {
+    if (src_char >= 0x20) {
       p_src_data += (60 * (src_char - 0x20));
+    } else {
+      teletext_handle_control_character(p_teletext, src_char);
     }
     p_src_data += (scanline * 6);
 
-    *p_dest_buffer++ = alpha;
-    *p_dest_buffer++ = alpha;
-    value = ((0x00ffffff * p_src_data[0]) | alpha);
+    bg_color = p_teletext->bg_color;
+    fg_color = p_teletext->fg_color;
+
+    *p_dest_buffer++ = bg_color;
+    *p_dest_buffer++ = bg_color;
+    value = (p_src_data[0] ? fg_color : bg_color);
     *p_dest_buffer++ = value;
     *p_dest_buffer++ = value;
-    value = ((0x00ffffff * p_src_data[1]) | alpha);
+    value = (p_src_data[1] ? fg_color : bg_color);
     *p_dest_buffer++ = value;
     *p_dest_buffer++ = value;
-    value = ((0x00ffffff * p_src_data[2]) | alpha);
+    value = (p_src_data[2] ? fg_color : bg_color);
     *p_dest_buffer++ = value;
     *p_dest_buffer++ = value;
-    value = ((0x00ffffff * p_src_data[3]) | alpha);
+    value = (p_src_data[3] ? fg_color : bg_color);
     *p_dest_buffer++ = value;
     *p_dest_buffer++ = value;
-    value = ((0x00ffffff * p_src_data[4]) | alpha);
+    value = (p_src_data[4] ? fg_color : bg_color);
     *p_dest_buffer++ = value;
     *p_dest_buffer++ = value;
-    value = ((0x00ffffff * p_src_data[5]) | alpha);
+    value = (p_src_data[5] ? fg_color : bg_color);
     *p_dest_buffer++ = value;
     *p_dest_buffer++ = value;
-    *p_dest_buffer++ = alpha;
-    *p_dest_buffer++ = alpha;
+    *p_dest_buffer++ = bg_color;
+    *p_dest_buffer++ = bg_color;
   }
 }
 
@@ -108,9 +209,9 @@ teletext_render_full(struct teletext_struct* p_teletext,
   for (row = 0; row < 25; ++row) {
     p_teletext->scanline = 0;
     for (scanline = 0; scanline < 10; ++scanline) {
-      teletext_render_line(p_video_mem, scanline, p_buffer);
+      teletext_render_line(p_teletext, p_video_mem, scanline, p_buffer);
       p_buffer += stride;
-      teletext_render_line(p_video_mem, scanline, p_buffer);
+      teletext_render_line(p_teletext, p_video_mem, scanline, p_buffer);
       p_buffer += stride;
     }
     p_video_mem += 40;
