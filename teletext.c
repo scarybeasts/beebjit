b2f0c1d7394c070d1259581a0ebdc09e2db6576a
Chris Evans <scarybeasts@gmail.com>
2019-11-07 00:13:13 -0800

    Add teletext double height and flash.

--- teletext.c
+++ teletext.c
@@ -19,13 +19,65 @@
 
 struct teletext_struct {
   uint32_t palette[8];
+  uint32_t flash_count;
+  int flash_visible_this_frame;
   uint32_t scanline;
   uint8_t* p_active_characters;
   int graphics_active;
   int separated_active;
+  int double_active;
+  int flash_active;
+  int had_double_active_this_scanline;
+  int second_character_row_of_double;
   uint32_t fg_color;
   uint32_t bg_color;
 };
+
+static void
+teletext_set_active_characters(struct teletext_struct* p_teletext) {
+  if (p_teletext->graphics_active) {
+    if (p_teletext->separated_active) {
+      p_teletext->p_active_characters = &teletext_separated_graphics[0];
+    } else {
+      p_teletext->p_active_characters = &teletext_graphics[0];
+    }
+  } else {
+    p_teletext->p_active_characters = &teletext_characters[0];
+  }
+}
+
+static void
+teletext_scanline_ended(struct teletext_struct* p_teletext) {
+  p_teletext->graphics_active = 0;
+  p_teletext->separated_active = 0;
+  p_teletext->double_active = 0;
+  p_teletext->flash_active = 0;
+  p_teletext->fg_color = p_teletext->palette[7];
+  p_teletext->bg_color = p_teletext->palette[0];
+
+  teletext_set_active_characters(p_teletext);
+
+  p_teletext->scanline++;
+  if (p_teletext->scanline == 10) {
+    p_teletext->scanline = 0;
+    if (p_teletext->second_character_row_of_double) {
+      p_teletext->second_character_row_of_double = 0;
+    } else if (p_teletext->had_double_active_this_scanline) {
+      p_teletext->second_character_row_of_double = 1;
+    }
+  }
+
+  p_teletext->had_double_active_this_scanline = 0;
+}
+
+static void
+teletext_frame_ended(struct teletext_struct* p_teletext) {
+  p_teletext->flash_count++;
+  if (p_teletext->flash_count == 48) {
+    p_teletext->flash_count = 0;
+  }
+  p_teletext->flash_visible_this_frame = (p_teletext->flash_count >= 16);
+}
 
 struct teletext_struct*
 teletext_create() {
@@ -37,6 +89,12 @@ teletext_create() {
   }
 
   (void) memset(p_teletext, '\0', sizeof(struct teletext_struct));
+
+  teletext_scanline_ended(p_teletext);
+  teletext_frame_ended(p_teletext);
+
+  p_teletext->flash_count = 0;
+  p_teletext->scanline = 0;
 
   for (i = 0; i < 8; ++i) {
     /* Black, full alpha. */
@@ -62,29 +120,6 @@ teletext_destroy(struct teletext_struct* p_teletext) {
 }
 
 static void
-teletext_set_active_characters(struct teletext_struct* p_teletext) {
-  if (p_teletext->graphics_active) {
-    if (p_teletext->separated_active) {
-      p_teletext->p_active_characters = &teletext_separated_graphics[0];
-    } else {
-      p_teletext->p_active_characters = &teletext_graphics[0];
-    }
-  } else {
-    p_teletext->p_active_characters = &teletext_characters[0];
-  }
-}
-
-void
-teletext_scanline_ended(struct teletext_struct* p_teletext) {
-  p_teletext->graphics_active = 0;
-  p_teletext->separated_active = 0;
-  p_teletext->fg_color = p_teletext->palette[7];
-  p_teletext->bg_color = p_teletext->palette[0];
-
-  teletext_set_active_characters(p_teletext);
-}
-
-static void
 teletext_handle_control_character(struct teletext_struct* p_teletext,
                                   uint8_t src_char) {
   switch (src_char) {
@@ -103,6 +138,19 @@ teletext_handle_control_character(struct teletext_struct* p_teletext,
   case 7:
     p_teletext->graphics_active = 0;
     p_teletext->fg_color = p_teletext->palette[src_char];
+    break;
+  case 8:
+    p_teletext->flash_active = 1;
+    break;
+  case 9:
+    p_teletext->flash_active = 0;
+    break;
+  case 12:
+    p_teletext->double_active = 0;
+    break;
+  case 13:
+    p_teletext->double_active = 1;
+    p_teletext->had_double_active_this_scanline = 1;
     break;
   case 16:
     /* Can't select black graphics -- see above. */
@@ -138,11 +186,11 @@ static void
 teletext_render_line(struct teletext_struct* p_teletext,
                      uint8_t* p_src_chars,
                      uint32_t scanline,
-                     uint32_t* p_dest_buffer) {
+                     uint32_t* p_dest_buffer,
+                     size_t stride) {
   uint32_t column;
-  uintptr_t src_chars = (uintptr_t) p_src_chars;
 
-  teletext_scanline_ended(p_teletext);
+  uintptr_t src_chars = (uintptr_t) p_src_chars;
 
   for (column = 0; column < 40; ++column) {
     uint8_t src_char;
@@ -151,6 +199,7 @@ teletext_render_line(struct teletext_struct* p_teletext,
     uint32_t fg_color;
     /* Selects space, 0x20. */
     uint8_t* p_src_data = p_teletext->p_active_characters;
+    uint32_t src_data_scanline = scanline;
 
     /* TODO: can abstract this. */
     if (src_chars & 0x8000) {
@@ -165,33 +214,73 @@ teletext_render_line(struct teletext_struct* p_teletext,
     } else {
       teletext_handle_control_character(p_teletext, src_char);
     }
-    p_src_data += (scanline * 6);
+
+    if (p_teletext->flash_active && !p_teletext->flash_visible_this_frame) {
+      /* Re-route to space. */
+      p_src_data = &teletext_characters[0];
+    }
+    if (p_teletext->double_active) {
+      src_data_scanline >>= 1;
+      if (p_teletext->second_character_row_of_double) {
+        src_data_scanline += 5;
+      }
+    }
+
+    p_src_data += (src_data_scanline * 6);
 
     bg_color = p_teletext->bg_color;
     fg_color = p_teletext->fg_color;
 
-    *p_dest_buffer++ = bg_color;
-    *p_dest_buffer++ = bg_color;
+    p_dest_buffer[0] = bg_color;
+    p_dest_buffer[1] = bg_color;
+    p_dest_buffer[stride + 0] = bg_color;
+    p_dest_buffer[stride + 1] = bg_color;
+
     value = (p_src_data[0] ? fg_color : bg_color);
-    *p_dest_buffer++ = value;
-    *p_dest_buffer++ = value;
+    p_dest_buffer[2] = value;
+    p_dest_buffer[3] = value;
+    p_dest_buffer[stride + 2] = value;
+    p_dest_buffer[stride + 3] = value;
+
     value = (p_src_data[1] ? fg_color : bg_color);
-    *p_dest_buffer++ = value;
-    *p_dest_buffer++ = value;
+    p_dest_buffer[4] = value;
+    p_dest_buffer[5] = value;
+    p_dest_buffer[stride + 4] = value;
+    p_dest_buffer[stride + 5] = value;
+
     value = (p_src_data[2] ? fg_color : bg_color);
-    *p_dest_buffer++ = value;
-    *p_dest_buffer++ = value;
+    p_dest_buffer[6] = value;
+    p_dest_buffer[7] = value;
+    p_dest_buffer[stride + 6] = value;
+    p_dest_buffer[stride + 7] = value;
+
     value = (p_src_data[3] ? fg_color : bg_color);
-    *p_dest_buffer++ = value;
-    *p_dest_buffer++ = value;
+    p_dest_buffer[8] = value;
+    p_dest_buffer[9] = value;
+    p_dest_buffer[stride + 8] = value;
+    p_dest_buffer[stride + 9] = value;
+
     value = (p_src_data[4] ? fg_color : bg_color);
-    *p_dest_buffer++ = value;
-    *p_dest_buffer++ = value;
+    p_dest_buffer[10] = value;
+    p_dest_buffer[11] = value;
+    p_dest_buffer[stride + 10] = value;
+    p_dest_buffer[stride + 11] = value;
+
     value = (p_src_data[5] ? fg_color : bg_color);
-    *p_dest_buffer++ = value;
-    *p_dest_buffer++ = value;
-    *p_dest_buffer++ = bg_color;
-    *p_dest_buffer++ = bg_color;
+    p_dest_buffer[12] = value;
+    p_dest_buffer[13] = value;
+    p_dest_buffer[stride + 12] = value;
+    p_dest_buffer[stride + 13] = value;
+
+    p_dest_buffer[14] = bg_color;
+    p_dest_buffer[15] = bg_color;
+    p_dest_buffer[stride + 14] = bg_color;
+    p_dest_buffer[stride + 15] = bg_color;
+
+    p_dest_buffer[14] = bg_color;
+    p_dest_buffer[15] = bg_color;
+
+    p_dest_buffer += 16;
   }
 }
 
@@ -209,11 +298,18 @@ teletext_render_full(struct teletext_struct* p_teletext,
   for (row = 0; row < 25; ++row) {
     p_teletext->scanline = 0;
     for (scanline = 0; scanline < 10; ++scanline) {
-      teletext_render_line(p_teletext, p_bbc_mem, scanline, p_render_buffer);
-      p_render_buffer += stride;
-      teletext_render_line(p_teletext, p_bbc_mem, scanline, p_render_buffer);
-      p_render_buffer += stride;
+      teletext_render_line(p_teletext,
+                           p_bbc_mem,
+                           scanline,
+                           p_render_buffer,
+                           stride);
+      teletext_scanline_ended(p_teletext);
+
+      p_render_buffer += (stride * 2);
     }
+
     p_bbc_mem += 40;
   }
+
+  teletext_frame_ended(p_teletext);
 }
