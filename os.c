168ee20801d6d24c528d4998c04cc6ff2c8e14b6
Chris Evans <scarybeasts@gmail.com>
2020-03-11 13:10:14 -0700

    Rename os_window_linux.c to os_window_x11.c.

--- os.c
+++ os.c
@@ -3,7 +3,7 @@
 #include "os_poller_linux.c"
 #include "os_sound_linux.c"
 #include "os_thread_linux.c"
-#include "os_window_linux.c"
+#include "os_window_x11.c"
 
 #else
 #error Not yet ported to Windows, Mac, etc.
