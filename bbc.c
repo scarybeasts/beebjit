1669c8041d66605e943d65c8121360d6d374c021
Chris Evans <scarybeasts@gmail.com>
2020-10-17 13:43:46 -0700

    Fix unintentional fall through.

--- bbc.c
+++ bbc.c
@@ -523,6 +523,7 @@ bbc_read_callback(void* p,
     } else {
       log_do_log(k_log_misc, k_log_unimplemented, "read of $FEC0-$FEDF region");
     }
+    break;
   case (k_addr_tube + 0):
   case (k_addr_tube + 4):
   case (k_addr_tube + 8):
