026f3dc207b033b6e3b3ae13e8a5eed2f9b51a9f
Chris Evans <chris@localhost.localdomain>
2020-12-20 21:15:06 -0800

    Add function missing from last commit.

--- os_fault_platform_null.c
+++ os_fault_platform_null.c
@@ -12,9 +12,14 @@ os_fault_get_jit_context(void* p) {
   return 0;
 }
 
+uintptr_t
+os_fault_get_pc(void* p) {
+  (void) p;
+  return 0;
+}
+
 void
 os_fault_set_pc(void* p, uintptr_t pc) {
   (void) p;
   (void) pc;
 }
-
