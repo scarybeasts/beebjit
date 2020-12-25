64d2ee8699560c50e462c925e15f222a54a28cbc
Chris Evans <scarybeasts@gmail.com>
2020-12-24 21:06:37 -0800

    Compile the asm files first.
    Speeds up work while I'm fiddling with ARM64.

--- build_opt.sh
+++ build_opt.sh
@@ -5,6 +5,11 @@ gcc -Wall -W -Werror \
     -Wno-unknown-warning-option -Wno-address-of-packed-member \
     -fno-pie -no-pie -Wa,--noexecstack \
     -O3 -DNDEBUG -o beebjit \
+    asm/asm_abi.c asm/asm_tables.c \
+    asm/asm_common.c asm/asm_common.S \
+    asm/asm_inturbo.c asm/asm_inturbo.S \
+    asm/asm_jit.c asm/asm_jit.S \
+    os.c \
     main.c config.c bbc.c defs_6502.c state.c video.c via.c \
     emit_6502.c interp.c inturbo.c state_6502.c sound.c timing.c \
     jit_compiler.c cpu_driver.c \
@@ -15,9 +20,4 @@ gcc -Wall -W -Werror \
     disc_fsd.c disc_hfe.c disc_ssd.c disc_adl.c \
     disc_rfi.c disc_kryo.c disc_scp.c \
     debug.c jit.c util.c \
-    os.c \
-    asm/asm_abi.c asm/asm_tables.c \
-    asm/asm_common.c asm/asm_common.S \
-    asm/asm_inturbo.c asm/asm_inturbo.S \
-    asm/asm_jit.c asm/asm_jit.S \
     -lm -lX11 -lXext -lpthread -lasound -lpulse -lpulse-simple
