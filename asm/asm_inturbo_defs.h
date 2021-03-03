361fd0403cb64f7c74a6f7d19660aa495b5a8081
Chris Evans <scarybeasts@gmail.com>
2021-03-02 17:17:37 -0800

    Use 128 byte code blocks for inturbo.
    rockets.ssd, RTRACE runs 1.45GHz -> 1.56GHz, +7.5%
    On AWS Graviton2, no noticeable change in CLOCKSP.
    
    Interesting study on the effects of L1 icache usage.
    The ARM chip has I think (at least?) 2x the L1 icache so maybe it is
    less sensitive to these things.

--- asm/asm_inturbo_defs.h
+++ asm/asm_inturbo_defs.h
@@ -2,7 +2,7 @@
 #define BEEBJIT_ASM_INTURBO_DEFS_H
 
 #define K_INTURBO_OPCODES                  0x40000000
-#define K_INTURBO_OPCODES_SHIFT            8
+#define K_INTURBO_OPCODES_SHIFT            7
 
 #endif /* BEEBJIT_ASM_INTURBO_DEFS_H */
 
