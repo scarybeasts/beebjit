846b88183efff92ad23e2ff08603e03af85f5fc9
Chris Evans <scarybeasts@gmail.com>
2021-03-24 20:22:45 -0700

    Fix opt build error.

--- expression.c
+++ expression.c
@@ -320,6 +320,7 @@ expression_parse(struct expression_struct* p_expression,
       i++;
     }
     token_len = 0;
+    c = p_expr_str[i];
     if (isalpha(c)) {
       while ((isalpha(c = p_expr_str[i]) || isdigit(c) || (c == '_')) &&
              (token_len < max_token_len)) {
