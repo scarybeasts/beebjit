7ec22cfdc5d556b5e3b64284bfa2f82bc3f1cf17
Chris Evans <scarybeasts@gmail.com>
2021-11-14 12:09:57 -0800

    Handle SIGBUS faults as well as SIGSEGV.
    macOS generates SIGBUS faults for writes to the read-only JIT
    mapping.

--- os_fault_posix.c
+++ os_fault_posix.c
@@ -10,7 +10,7 @@
 static void (*s_p_fault_callback)(uintptr_t*, uintptr_t, int, int, uintptr_t);
 
 static void
-linux_sigsegv_handler(int signum, siginfo_t* p_siginfo, void* p_void) {
+posix_fault_handler(int signum, siginfo_t* p_siginfo, void* p_void) {
   uintptr_t host_fault_addr;
   uintptr_t host_pc;
   uintptr_t host_context;
@@ -18,7 +18,14 @@ linux_sigsegv_handler(int signum, siginfo_t* p_siginfo, void* p_void) {
   int is_write_fault;
 
   /* Crash unless it's fault type we expected. */
-  if (signum != SIGSEGV || p_siginfo->si_code != SEGV_ACCERR) {
+  if ((signum == SIGSEGV) && (p_siginfo->si_code == SEGV_ACCERR)) {
+    /* OK. */
+  } else if ((signum == SIGBUS) && (p_siginfo->si_code == BUS_ADRALN)) {
+    /* OK; hits on macOS for writing to the read-only JIT mapping.
+     * Note that it's definitely not a bad alignment; that's just the code that
+     * comes through as the others would likely make even less sense.
+     */
+  } else {
     os_fault_bail();
   }
 
@@ -37,6 +44,27 @@ linux_sigsegv_handler(int signum, siginfo_t* p_siginfo, void* p_void) {
   os_fault_set_pc(p_void, host_pc);
 }
 
+static void
+install_handler(int signal) {
+  struct sigaction sa;
+  struct sigaction sa_prev;
+  int ret;
+
+  (void) memset(&sa, '\0', sizeof(sa));
+  (void) memset(&sa_prev, '\0', sizeof(sa_prev));
+
+  sa.sa_sigaction = posix_fault_handler;
+  sa.sa_flags = (SA_SIGINFO | SA_NODEFER);
+  ret = sigaction(signal, &sa, &sa_prev);
+  if (ret != 0) {
+    util_bail("sigaction failed");
+  }
+  if ((sa_prev.sa_sigaction != NULL) &&
+      (sa_prev.sa_sigaction != posix_fault_handler)) {
+    util_bail("conflicting fault handler");
+  }
+}
+
 void
 os_fault_register_handler(
     void (*p_fault_callback)(uintptr_t* p_host_rip,
@@ -44,25 +72,11 @@ os_fault_register_handler(
                              int is_exec,
                              int is_write,
                              uintptr_t host_rdi)) {
-  struct sigaction sa;
-  struct sigaction sa_prev;
-  int ret;
-
   s_p_fault_callback = p_fault_callback;
 
-  (void) memset(&sa, '\0', sizeof(sa));
-  (void) memset(&sa_prev, '\0', sizeof(sa_prev));
-
-  sa.sa_sigaction = linux_sigsegv_handler;
-  sa.sa_flags = (SA_SIGINFO | SA_NODEFER);
-  ret = sigaction(SIGSEGV, &sa, &sa_prev);
-  if (ret != 0) {
-    util_bail("sigaction failed");
-  }
-  if ((sa_prev.sa_sigaction != NULL) &&
-      (sa_prev.sa_sigaction != linux_sigsegv_handler)) {
-    util_bail("conflicting SIGSEGV handler");
-  }
+  install_handler(SIGSEGV);
+  /* On macOS, a write fault to our JIT mapping comes in as SIGBUS. */
+  install_handler(SIGBUS);
 }
 
 void
