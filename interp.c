scarybeasts/beebjit

commit 7faa004dc219539b714d56f2c1f4b7e496c755a3

Fill in opcodes 0x3x.

interp.c
@@ -565,6 +565,13 @@ interp_check_log_bcd(struct interp_struct* p_interp) {
     goto check_irq;                                                           \
   }
 
+#define INTERP_MODE_ZPG_READ(INSTR)                                           \
+  addr = p_mem_read[pc + 1];                                                  \
+  pc += 2;                                                                    \
+  v = p_mem_read[addr];                                                       \
+  INSTR;                                                                      \
+  cycles_this_instruction = 3;
+
 #define INTERP_MODE_ZPG_READ_WRITE(INSTR)                                     \
   addr = p_mem_read[pc + 1];                                                  \
   pc += 2;                                                                    \
@@ -572,6 +579,13 @@ interp_check_log_bcd(struct interp_struct* p_interp) {
   INSTR;                                                                      \
   p_mem_write[addr] = v;                                                      \
   cycles_this_instruction = 5;
+
+#define INTERP_MODE_ZPG_WRITE(INSTR)                                          \
+  addr = p_mem_read[pc + 1];                                                  \
+  pc += 2;                                                                    \
+  INSTR;                                                                      \
+  p_mem_write[addr] = v;                                                      \
+  cycles_this_instruction = 3;
 
 #define INTERP_MODE_ZPr_READ(reg_name)                                        \
   addr = p_mem_read[pc + 1];                                                  \
@@ -583,19 +597,21 @@ interp_check_log_bcd(struct interp_struct* p_interp) {
 
 #define INTERP_MODE_ZPr_WRITE(reg_name)                                       \
   addr = p_mem_read[pc + 1];                                                  \
+  pc += 2;                                                                    \
   addr += reg_name;                                                           \
   addr &= 0xFF;                                                               \
   p_mem_write[addr] = v;                                                      \
-  cycles_this_instruction = 4;                                                \
-  pc += 2;
+  cycles_this_instruction = 4;
 
-#define INTERP_MODE_ZPX_READ_WRITE()                                          \
+#define INTERP_MODE_ZPX_READ_WRITE(INSTR)                                     \
   addr = p_mem_read[pc + 1];                                                  \
+  pc += 2;                                                                    \
   addr += x;                                                                  \
   addr &= 0xFF;                                                               \
   v = p_mem_read[addr];                                                       \
-  cycles_this_instruction = 6;                                                \
-  pc += 2;
+  INSTR;                                                                      \
+  p_mem_write[addr] = v;                                                      \
+  cycles_this_instruction = 5;
 
 #define INTERP_LOAD_NZ_FLAGS(reg_name)                                        \
   nf = !!(reg_name & 0x80);                                                   \
@@ -958,16 +974,13 @@ interp_enter_with_details(struct interp_struct* p_interp,
     case 0x06: /* ASL zpg */
       INTERP_MODE_ZPG_READ_WRITE(INTERP_INSTR_ASL());
       break;
-    case 0x07: /* SLO zpg */ /* Undocumented. */
-      addr = p_mem_read[pc + 1];
-      v = p_mem_read[addr];
-      cf = !!(v & 0x80);
-      v <<= 1;
-      p_mem_write[addr] = v;
-      a |= v;
-      INTERP_LOAD_NZ_FLAGS(a);
-      pc += 2;
-      cycles_this_instruction = 5;
+    case 0x07: /* SLO zpg */ /* Undocumented. */ /* NOP1 */
+      if (is_65c12) {
+        pc++;
+        cycles_this_instruction = 1;
+      } else {
+        INTERP_MODE_ZPG_READ_WRITE(INTERP_INSTR_SLO());
+      }
       break;
     case 0x08: /* PHP */
       v = interp_get_flags(zf, nf, cf, of, df, intf);
@@ -1052,6 +1065,14 @@ interp_enter_with_details(struct interp_struct* p_interp,
       INTERP_INSTR_ASL();
       p_mem_write[addr] = v;
       break;
+    case 0x17: /* SLO zpx */ /* Undocumented. */ /* NOP1 */
+      if (is_65c12) {
+        pc++;
+        cycles_this_instruction = 1;
+      } else {
+        INTERP_MODE_ZPX_READ_WRITE(INTERP_INSTR_SLO());
+      }
+      break;
     case 0x18: /* CLC */
       cf = 0;
       pc++;
@@ -1132,6 +1153,14 @@ interp_enter_with_details(struct interp_struct* p_interp,
     case 0x26: /* ROL zpg */
       INTERP_MODE_ZPG_READ_WRITE(INTERP_INSTR_ROL());
       break;
+    case 0x27: /* RLA zpg */ /* Undocumented. */ /* NOP1 */
+      if (is_65c12) {
+        pc++;
+        cycles_this_instruction = 1;
+      } else {
+        INTERP_MODE_ZPG_READ_WRITE(INTERP_INSTR_RLA());
+      }
+      break;
     case 0x28: /* PLP */
       /* PLP fiddles with the interrupt disable flag so we need to tick it
        * out to get the correct ordering and behavior.
@@ -1208,6 +1237,14 @@ interp_enter_with_details(struct interp_struct* p_interp,
       INTERP_MODE_ZPX_READ_WRITE();
       INTERP_INSTR_ROL();
       p_mem_write[addr] = v;
+      break;
+    case 0x37: /* RLA zpx */ /* Undocumented. */ /* NOP1 */
+      if (is_65c12) {
+        pc++;
+        cycles_this_instruction = 1;
+      } else {
+        INTERP_MODE_ZPX_READ_WRITE(INTERP_INSTR_RLA());
+      }
       break;
     case 0x38: /* SEC */
       cf = 1;
@@ -1290,13 +1327,13 @@ interp_enter_with_details(struct interp_struct* p_interp,
     case 0x46: /* LSR zpg */
       INTERP_MODE_ZPG_READ_WRITE(INTERP_INSTR_LSR());
       break;
-    case 0x47: /* SRE zpg */ /* Undocumented. */
-      addr = p_mem_read[pc + 1];
-      v = p_mem_read[addr];
-      INTERP_INSTR_SRE();
-      p_mem_write[addr] = v;
-      pc += 2;
-      cycles_this_instruction = 5;
+    case 0x47: /* SRE zpg */ /* Undocumented. */ /* NOP1 */
+      if (is_65c12) {
+        pc++;
+        cycles_this_instruction = 1;
+      } else {
+        INTERP_MODE_ZPG_READ_WRITE(INTERP_INSTR_SRE());
+      }
       break;
     case 0x48: /* PHA */
       p_stack[s--] = a;
@@ -1374,6 +1411,14 @@ interp_enter_with_details(struct interp_struct* p_interp,
       INTERP_MODE_ZPX_READ_WRITE();
       INTERP_INSTR_LSR();
       p_mem_write[addr] = v;
+      break;
+    case 0x57: /* SRE zpx */ /* Undocumented. */ /* NOP1 */
+      if (is_65c12) {
+        pc++;
+        cycles_this_instruction = 1;
+      } else {
+        INTERP_MODE_ZPX_READ_WRITE(INTERP_INSTR_SRE());
+      }
       break;
     case 0x58: /* CLI */
       /* CLI enables interrupts but this takes effect after the IRQ poll
@@ -1460,6 +1505,14 @@ interp_enter_with_details(struct interp_struct* p_interp,
     case 0x66: /* ROR zpg */
       INTERP_MODE_ZPG_READ_WRITE(INTERP_INSTR_ROR());
       break;
+    case 0x67: /* RRA zpg */ /* Undocumented. */ /* NOP1 */
+      if (is_65c12) {
+        pc++;
+        cycles_this_instruction = 1;
+      } else {
+        INTERP_MODE_ZPG_READ_WRITE(INTERP_INSTR_RRA());
+      }
+      break;
     case 0x68: /* PLA */
       a = p_stack[++s];
       INTERP_LOAD_NZ_FLAGS(a);
@@ -1539,6 +1592,14 @@ interp_enter_with_details(struct interp_struct* p_interp,
       INTERP_MODE_ZPX_READ_WRITE();
       INTERP_INSTR_ROR();
       p_mem_write[addr] = v;
+      break;
+    case 0x77: /* RRA zpx */ /* Undocumented. */ /* NOP1 */
+      if (is_65c12) {
+        pc++;
+        cycles_this_instruction = 1;
+      } else {
+        INTERP_MODE_ZPX_READ_WRITE(INTERP_INSTR_RRA());
+      }
       break;
     case 0x78: /* SEI */
       /* SEI disables interrupts but this takes effect after the IRQ poll
@@ -1626,11 +1687,13 @@ interp_enter_with_details(struct interp_struct* p_interp,
       pc += 2;
       cycles_this_instruction = 3;
       break;
-    case 0x87: /* SAX zpg */ /* Undocumented. */
-      addr = p_mem_read[pc + 1];
-      p_mem_write[addr] = (a & x);
-      pc += 2;
-      cycles_this_instruction = 3;
+    case 0x87: /* SAX zpg */ /* Undocumented. */ /* NOP1 */
+      if (is_65c12) {
+        pc++;
+        cycles_this_instruction = 1;
+      } else {
+        INTERP_MODE_ZPG_WRITE(INTERP_INSTR_SAX());
+      }
       break;
     case 0x88: /* DEY */
       y--;
@@ -1713,6 +1776,15 @@ interp_enter_with_details(struct interp_struct* p_interp,
       v = x;
       INTERP_MODE_ZPr_WRITE(y);
       break;
+    case 0x97: /* SAX zpy */ /* Undocumented. */ /* NOP1 */
+      if (is_65c12) {
+        pc++;
+        cycles_this_instruction = 1;
+      } else {
+        INTERP_INSTR_SAX();
+        INTERP_MODE_ZPr_WRITE(y);
+      }
+      break;
     case 0x98: /* TYA */
       a = y;
       INTERP_LOAD_NZ_FLAGS(a);
@@ -1788,13 +1860,13 @@ interp_enter_with_details(struct interp_struct* p_interp,
       pc += 2;
       cycles_this_instruction = 3;
       break;
-    case 0xA7: /* LAX zpg */ /* Undocumented. */
-      addr = p_mem_read[pc + 1];
-      a = p_mem_read[addr];
-      x = a;
-      INTERP_LOAD_NZ_FLAGS(a);
-      pc += 2;
-      cycles_this_instruction = 3;
+    case 0xA7: /* LAX zpg */ /* Undocumented. */ /* NOP1 */
+      if (is_65c12) {
+        pc++;
+        cycles_this_instruction = 1;
+      } else {
+        INTERP_MODE_ZPG_READ(INTERP_INSTR_LAX());
+      }
       break;
     case 0xA8: /* TAY */
       y = a;
@@ -1872,6 +1944,15 @@ interp_enter_with_details(struct interp_struct* p_interp,
       x = v;
       INTERP_LOAD_NZ_FLAGS(x);
       break;
+    case 0xB7: /* LAX zpy */ /* Undocumented. */ /* NOP1 */
+      if (is_65c12) {
+        pc++;
+        cycles_this_instruction = 1;
+      } else {
+        INTERP_MODE_ZPr_READ(y);
+        INTERP_INSTR_LAX();
+      }
+      break;
     case 0xB8: /* CLV */
       of = 0;
       pc++;
@@ -1928,6 +2009,14 @@ interp_enter_with_details(struct interp_struct* p_interp,
       break;
     case 0xC6: /* DEC zpg */
       INTERP_MODE_ZPG_READ_WRITE(INTERP_INSTR_DEC());
+      break;
+    case 0xC7: /* DCP zpg */ /* Undocumented. */ /* NOP1 */
+      if (is_65c12) {
+        pc++;
+        cycles_this_instruction = 1;
+      } else {
+        INTERP_MODE_ZPG_READ_WRITE(INTERP_INSTR_DCP());
+      }
       break;
     case 0xC8: /* INY */
       y++;
@@ -1996,6 +2085,14 @@ interp_enter_with_details(struct interp_struct* p_interp,
       p_mem_write[addr] = v;
       INTERP_LOAD_NZ_FLAGS(v);
       break;
+    case 0xD7: /* DCP zpx */ /* Undocumented. */ /* NOP1 */
+      if (is_65c12) {
+        pc++;
+        cycles_this_instruction = 1;
+      } else {
+        INTERP_MODE_ZPX_READ_WRITE(INTERP_INSTR_DCP());
+      }
+      break;
     case 0xD8: /* CLD */
       df = 0;
       pc++;
@@ -2062,6 +2159,14 @@ interp_enter_with_details(struct interp_struct* p_interp,
     case 0xE6: /* INC zpg */
       INTERP_MODE_ZPG_READ_WRITE(INTERP_INSTR_INC());
       break;
+    case 0xE7: /* ISC zpg */ /* Undocumented. */ /* NOP1 */
+      if (is_65c12) {
+        pc++;
+        cycles_this_instruction = 1;
+      } else {
+        util_bail("ISC");
+      }
+      break;
     case 0xE8: /* INX */
       x++;
       INTERP_LOAD_NZ_FLAGS(x);
@@ -2118,6 +2223,14 @@ interp_enter_with_details(struct interp_struct* p_interp,
       v++;
       p_mem_write[addr] = v;
       INTERP_LOAD_NZ_FLAGS(v);
+      break;
+    case 0xF7: /* ISC zpx */ /* Undocumented. */ /* NOP1 */
+      if (is_65c12) {
+        pc++;
+        cycles_this_instruction = 1;
+      } else {
+        util_bail("ISC");
+      }
       break;
     case 0xF8: /* SED */
       df = 1;

defs_6502.c
@@ -12,8 +12,8 @@ const char* g_p_opnames[k_6502_op_num_types] =
   "TAX", "BCS", "CLV", "TSX", "CPY", "CMP", "CPX", "DEC",
   "INY", "DEX", "BNE", "CLD", "SBC", "INX", "NOP", "INC",
   "BEQ", "SED", "SAX", "ALR", "SLO", "SHY", "ANC", "LAX",
-  "DCP", "SRE", "RLA", "AHX", "XAA", "RRA", "AXS", "TSB",
-  "TRB", "STZ", "BRA", "PHX", "PHY", "PLX", "PLY",
+  "DCP", "SRE", "RLA", "AHX", "XAA", "RRA", "AXS", "ISC",
+  "TSB", "TRB", "STZ", "BRA", "PHX", "PHY", "PLX", "PLY",
 };
 
 uint8_t g_opmem[k_6502_op_num_types] = {
@@ -26,7 +26,7 @@ uint8_t g_opmem[k_6502_op_num_types] = {
   k_nomem, k_nomem, k_nomem, k_nomem, k_read , k_nomem, k_read , k_rw   ,
   k_nomem, k_nomem, k_write, k_nomem, k_rw   , k_write, k_nomem, k_read ,
   k_rw   , k_rw   , k_rw   , k_write, k_nomem, k_rw   , k_nomem, k_rw   ,
-  k_rw   , k_write, k_nomem, k_nomem, k_nomem, k_nomem, k_nomem,
+  k_rw   , k_rw   , k_write, k_nomem, k_nomem, k_nomem, k_nomem, k_nomem,
 };
 
 uint8_t g_opbranch[k_6502_op_num_types] = {
@@ -39,7 +39,7 @@ uint8_t g_opbranch[k_6502_op_num_types] = {
   k_bra_n, k_bra_n, k_bra_m, k_bra_n, k_bra_n, k_bra_n, k_bra_n, k_bra_n,
   k_bra_m, k_bra_n, k_bra_n, k_bra_n, k_bra_n, k_bra_n, k_bra_n, k_bra_n,
   k_bra_n, k_bra_n, k_bra_n, k_bra_n, k_bra_n, k_bra_n, k_bra_n, k_bra_n,
-  k_bra_n, k_bra_n, k_bra_y, k_bra_n, k_bra_n, k_bra_n, k_bra_n,
+  k_bra_n, k_bra_n, k_bra_n, k_bra_y, k_bra_n, k_bra_n, k_bra_n, k_bra_n,
 };
 
 uint8_t g_optype_uses_carry[k_6502_op_num_types] = {
@@ -52,7 +52,7 @@ uint8_t g_optype_uses_carry[k_6502_op_num_types] = {
   0, 0, 0, 0, 1, 0, 0, 0, /* SBC */
   0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 1, 0, 0, 0, 0, 0, /* RLA */
-  0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
 };
 
 uint8_t g_optype_changes_carry[k_6502_op_num_types] = {
@@ -64,8 +64,8 @@ uint8_t g_optype_changes_carry[k_6502_op_num_types] = {
   0, 0, 0, 0, 1, 1, 1, 0, /* CPY, CMP, CPX */
   0, 0, 0, 0, 1, 0, 0, 0, /* SBC */
   0, 0, 0, 1, 1, 0, 1, 0, /* ALR, SLO, ANC */
-  1, 1, 1, 0, 0, 1, 1, 0, /* DCP, SRE, RLA, RRA, AXS */
-  0, 0, 0, 0, 0, 0, 0,
+  1, 1, 1, 0, 0, 1, 1, 1, /* DCP, SRE, RLA, RRA, AXS, ISC */
+  0, 0, 0, 0, 0, 0, 0, 0,
 };
 
 uint8_t g_optype_changes_overflow[k_6502_op_num_types] = {
@@ -77,8 +77,8 @@ uint8_t g_optype_changes_overflow[k_6502_op_num_types] = {
   0, 0, 1, 0, 0, 0, 0, 0, /* CLV */
   0, 0, 0, 0, 1, 0, 0, 0, /* SBC */
   0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 1, 0, 0, /* RRA */
-  0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 1, 0, 1, /* RRA, ISC */
+  0, 0, 0, 0, 0, 0, 0, 0,
 };
 
 /* TODO: need k_ax for LAX?? */
@@ -92,8 +92,8 @@ uint8_t g_optype_sets_register[k_6502_op_num_types] =
   k_x, 0  , 0  , k_x, 0  , 0  , 0  , 0  , /* TAX, TSX */
   k_y, k_x, 0  , 0  , k_a, k_x, 0  , 0  , /* INY, DEX, SBC, INX */
   0  , 0  , 0  , k_a, k_a, 0  , k_a, k_a, /* ALR, SLO, ANC, LAX */
-  0  , k_a, k_a, 0  , k_a, k_a, k_x, 0  , /* SRE, RLA, XAA, RRA, AXS */
-  0  , 0  , 0  , 0  , 0  , k_x, k_y,      /* PLX, PLY */
+  0  , k_a, k_a, 0  , k_a, k_a, k_x, k_a, /* SRE, RLA, XAA, RRA, AXS, ISC */
+  0  , 0  , 0  , 0  , 0  , 0  , k_x, k_y, /* PLX, PLY */
 };
 
 /* NOTE: TSB, TRB only change Z. */
@@ -107,8 +107,8 @@ uint8_t g_optype_changes_nz_flags[k_6502_op_num_types] =
   1, 0, 0, 1, 1, 1, 1, 1, /* TAX, TSX, CPY, CMP, CPX, DEC */
   1, 1, 0, 0, 1, 1, 0, 1, /* INY, DEX, SBC, INX, INC */
   0, 0, 0, 1, 1, 0, 1, 1, /* ALR, SLO, ANC, LAX */
-  1, 1, 1, 0, 1, 1, 1, 1, /* DCP, SRE, RLA, XAA, RRA, AXS, TSB */
-  1, 0, 0, 0, 0, 1, 1,    /* TRB, PLX, PLY */
+  1, 1, 1, 0, 1, 1, 1, 1, /* DCP, SRE, RLA, XAA, RRA, AXS, ISC */
+  1, 1, 0, 0, 0, 0, 1, 1, /* TSB, TRB, PLX, PLY */
 };
 
 uint8_t s_optypes_6502[k_6502_op_num_opcodes] =
@@ -117,49 +117,49 @@ uint8_t s_optypes_6502[k_6502_op_num_opcodes] =
   k_brk, k_ora, k_kil, k_slo, k_nop, k_ora, k_asl, k_slo,
   k_php, k_ora, k_asl, k_anc, k_nop, k_ora, k_asl, k_slo,
   /* 0x10 */
-  k_bpl, k_ora, k_kil, k_unk, k_nop, k_ora, k_asl, k_unk,
+  k_bpl, k_ora, k_kil, k_unk, k_nop, k_ora, k_asl, k_slo,
   k_clc, k_ora, k_nop, k_unk, k_nop, k_ora, k_asl, k_unk,
   /* 0x20 */
-  k_jsr, k_and, k_kil, k_rla, k_bit, k_and, k_rol, k_unk,
+  k_jsr, k_and, k_kil, k_rla, k_bit, k_and, k_rol, k_rla,
   k_plp, k_and, k_rol, k_unk, k_bit, k_and, k_rol, k_rla,
   /* 0x30 */
-  k_bmi, k_and, k_kil, k_rla, k_nop, k_and, k_rol, k_unk,
+  k_bmi, k_and, k_kil, k_rla, k_nop, k_and, k_rol, k_rla,
   k_sec, k_and, k_nop, k_unk, k_nop, k_and, k_rol, k_unk,
   /* 0x40 */
   k_rti, k_eor, k_kil, k_unk, k_nop, k_eor, k_lsr, k_sre,
   k_pha, k_eor, k_lsr, k_alr, k_jmp, k_eor, k_lsr, k_sre,
   /* 0x50 */
-  k_bvc, k_eor, k_kil, k_unk, k_nop, k_eor, k_lsr, k_unk,
+  k_bvc, k_eor, k_kil, k_unk, k_nop, k_eor, k_lsr, k_sre,
   k_cli, k_eor, k_nop, k_unk, k_nop, k_eor, k_lsr, k_unk,
   /* 0x60 */
-  k_rts, k_adc, k_kil, k_unk, k_nop, k_adc, k_ror, k_unk,
+  k_rts, k_adc, k_kil, k_unk, k_nop, k_adc, k_ror, k_rra,
   k_pla, k_adc, k_ror, k_unk, k_jmp, k_adc, k_ror, k_rra,
   /* 0x70 */
-  k_bvs, k_adc, k_kil, k_rra, k_nop, k_adc, k_ror, k_unk,
+  k_bvs, k_adc, k_kil, k_rra, k_nop, k_adc, k_ror, k_rra,
   k_sei, k_adc, k_nop, k_unk, k_nop, k_adc, k_ror, k_unk,
   /* 0x80 */
   k_nop, k_sta, k_nop, k_sax, k_sty, k_sta, k_stx, k_sax,
   k_dey, k_nop, k_txa, k_xaa, k_sty, k_sta, k_stx, k_sax,
   /* 0x90 */
-  k_bcc, k_sta, k_kil, k_ahx, k_sty, k_sta, k_stx, k_unk,
+  k_bcc, k_sta, k_kil, k_ahx, k_sty, k_sta, k_stx, k_sax,
   k_tya, k_sta, k_txs, k_unk, k_shy, k_sta, k_unk, k_unk,
   /* 0xa0 */
   k_ldy, k_lda, k_ldx, k_unk, k_ldy, k_lda, k_ldx, k_lax,
   k_tay, k_lda, k_tax, k_lax, k_ldy, k_lda, k_ldx, k_lax,
   /* 0xb0 */
-  k_bcs, k_lda, k_kil, k_unk, k_ldy, k_lda, k_ldx, k_unk,
+  k_bcs, k_lda, k_kil, k_unk, k_ldy, k_lda, k_ldx, k_lax,
   k_clv, k_lda, k_tsx, k_unk, k_ldy, k_lda, k_ldx, k_unk,
   /* 0xc0 */
-  k_cpy, k_cmp, k_nop, k_unk, k_cpy, k_cmp, k_dec, k_unk,
+  k_cpy, k_cmp, k_nop, k_unk, k_cpy, k_cmp, k_dec, k_dcp,
   k_iny, k_cmp, k_dex, k_axs, k_cpy, k_cmp, k_dec, k_unk,
   /* 0xd0 */
-  k_bne, k_cmp, k_kil, k_dcp, k_nop, k_cmp, k_dec, k_unk,
+  k_bne, k_cmp, k_kil, k_dcp, k_nop, k_cmp, k_dec, k_dcp,
   k_cld, k_cmp, k_nop, k_unk, k_nop, k_cmp, k_dec, k_unk,
   /* 0xe0 */
-  k_cpx, k_sbc, k_nop, k_unk, k_cpx, k_sbc, k_inc, k_unk,
+  k_cpx, k_sbc, k_nop, k_unk, k_cpx, k_sbc, k_inc, k_isc,
   k_inx, k_sbc, k_nop, k_sbc, k_cpx, k_sbc, k_inc, k_unk,
   /* 0xf0 */
-  k_beq, k_sbc, k_kil, k_unk, k_nop, k_sbc, k_inc, k_unk,
+  k_beq, k_sbc, k_kil, k_unk, k_nop, k_sbc, k_inc, k_isc,
   k_sed, k_sbc, k_nop, k_unk, k_nop, k_sbc, k_inc, k_unk,
 };
 
@@ -169,49 +169,49 @@ uint8_t s_opmodes_6502[k_6502_op_num_opcodes] =
   k_imm, k_idx, 0    , k_idx, k_zpg, k_zpg, k_zpg, k_zpg,
   k_nil, k_imm, k_acc, k_imm, k_abs, k_abs, k_abs, k_abs,
   /* 0x10 */
-  k_rel, k_idy, 0    , 0    , k_zpx, k_zpx, k_zpx, 0    ,
+  k_rel, k_idy, 0    , 0    , k_zpx, k_zpx, k_zpx, k_zpx,
   k_nil, k_aby, k_nil, 0    , k_abx, k_abx, k_abx, 0    ,
   /* 0x20 */
-  k_abs, k_idx, 0    , k_idx, k_zpg, k_zpg, k_zpg, 0    ,
+  k_abs, k_idx, 0    , k_idx, k_zpg, k_zpg, k_zpg, k_zpg,
   k_nil, k_imm, k_acc, 0    , k_abs, k_abs, k_abs, k_abs,
   /* 0x30 */
-  k_rel, k_idy, 0    , k_idy, k_zpx, k_zpx, k_zpx, 0    ,
+  k_rel, k_idy, 0    , k_idy, k_zpx, k_zpx, k_zpx, k_zpx,
   k_nil, k_aby, k_nil, 0    , k_abx, k_abx, k_abx, 0    ,
   /* 0x40 */
   k_nil, k_idx, 0    , 0    , k_zpg, k_zpg, k_zpg, k_zpg,
   k_nil, k_imm, k_acc, k_imm, k_abs, k_abs, k_abs, k_abs,
   /* 0x50 */
-  k_rel, k_idy, 0    , 0    , k_zpx, k_zpx, k_zpx, 0    ,
+  k_rel, k_idy, 0    , 0    , k_zpx, k_zpx, k_zpx, k_zpx,
   k_nil, k_aby, k_nil, 0    , k_abx, k_abx, k_abx, 0    ,
   /* 0x60 */
-  k_nil, k_idx, 0    , 0    , k_zpg, k_zpg, k_zpg, 0    ,
+  k_nil, k_idx, 0    , 0    , k_zpg, k_zpg, k_zpg, k_zpg,
   k_nil, k_imm, k_acc, 0    , k_ind, k_abs, k_abs, k_abs,
   /* 0x70 */
-  k_rel, k_idy, 0    , k_idy, k_zpx, k_zpx, k_zpx, 0    ,
+  k_rel, k_idy, 0    , k_idy, k_zpx, k_zpx, k_zpx, k_zpx,
   k_nil, k_aby, k_nil, 0    , k_abx, k_abx, k_abx, 0    ,
   /* 0x80 */
   k_imm, k_idx, k_imm, k_idx, k_zpg, k_zpg, k_zpg, k_zpg,
   k_nil, k_imm, k_nil, k_imm, k_abs, k_abs, k_abs, k_abs,
   /* 0x90 */
-  k_rel, k_idy, 0    , k_idy, k_zpx, k_zpx, k_zpy, 0    ,
+  k_rel, k_idy, 0    , k_idy, k_zpx, k_zpx, k_zpy, k_zpy,
   k_nil, k_aby, k_nil, 0    , k_abx, k_abx, 0    , 0    ,
   /* 0xa0 */
   k_imm, k_idx, k_imm, 0    , k_zpg, k_zpg, k_zpg, k_zpg,
   k_nil, k_imm, k_nil, k_imm, k_abs, k_abs, k_abs, k_abs,
   /* 0xb0 */
-  k_rel, k_idy, 0    , 0    , k_zpx, k_zpx, k_zpy, 0    ,
+  k_rel, k_idy, 0    , 0    , k_zpx, k_zpx, k_zpy, k_zpy,
   k_nil, k_aby, k_nil, 0    , k_abx, k_abx, k_aby, 0    ,
   /* 0xc0 */
-  k_imm, k_idx, k_imm, 0    , k_zpg, k_zpg, k_zpg, 0    ,
+  k_imm, k_idx, k_imm, 0    , k_zpg, k_zpg, k_zpg, k_zpg,
   k_nil, k_imm, k_nil, k_imm, k_abs, k_abs, k_abs, 0    ,
   /* 0xd0 */
-  k_rel, k_idy, 0    , k_idy, k_zpx, k_zpx, k_zpx, 0    ,
+  k_rel, k_idy, 0    , k_idy, k_zpx, k_zpx, k_zpx, k_zpx,
   k_nil, k_aby, k_nil, 0    , k_abx, k_abx, k_abx, 0    ,
   /* 0xe0 */
-  k_imm, k_idx, k_imm, 0    , k_zpg, k_zpg, k_zpg, 0    ,
+  k_imm, k_idx, k_imm, 0    , k_zpg, k_zpg, k_zpg, k_zpg,
   k_nil, k_imm, k_nil, k_imm, k_abs, k_abs, k_abs, 0    ,
   /* 0xf0 */
-  k_rel, k_idy, 0    , 0    , k_zpx, k_zpx, k_zpx, 0    ,
+  k_rel, k_idy, 0    , 0    , k_zpx, k_zpx, k_zpx, k_zpx,
   k_nil, k_aby, k_nil, 0    , k_abx, k_abx, k_abx, 0    ,
 };
 
@@ -221,49 +221,49 @@ uint8_t s_opcycles_6502[k_6502_op_num_opcodes] =
   7, 6, 1, 8, 3, 3, 5, 5,
   3, 2, 2, 2, 4, 4, 6, 6,
   /* 0x10 */
-  2, 5, 1, 0, 4, 4, 6, 0,
+  2, 5, 1, 0, 4, 4, 6, 6,
   2, 4, 2, 0, 4, 4, 7, 0,
   /* 0x20 */
-  6, 6, 1, 8, 3, 3, 5, 0,
+  6, 6, 1, 8, 3, 3, 5, 5,
   4, 2, 2, 0, 4, 4, 6, 6,
   /* 0x30 */
-  2, 5, 0, 8, 4, 4, 6, 0,
+  2, 5, 0, 8, 4, 4, 6, 6,
   2, 4, 2, 0, 4, 4, 7, 0,
   /* 0x40 */
   6, 6, 0, 0, 3, 3, 5, 5,
   3, 2, 2, 2, 3, 4, 6, 6,
   /* 0x50 */
-  2, 5, 0, 0, 4, 4, 6, 0,
+  2, 5, 0, 0, 4, 4, 6, 6,
   2, 4, 2, 0, 4, 4, 7, 0,
   /* 0x60 */
-  6, 6, 0, 0, 3, 3, 5, 0,
+  6, 6, 0, 0, 3, 3, 5, 5,
   4, 2, 2, 0, 5, 4, 6, 6,
   /* 0x70 */
-  2, 5, 0, 8, 4, 4, 6, 0,
+  2, 5, 0, 8, 4, 4, 6, 6,
   2, 4, 2, 0, 4, 4, 7, 0,
   /* 0x80 */
   2, 6, 2, 6, 3, 3, 3, 3,
   2, 2, 2, 2, 4, 4, 4, 4,
   /* 0x90 */
-  2, 6, 0, 6, 4, 4, 4, 0,
+  2, 6, 0, 6, 4, 4, 4, 4,
   2, 5, 2, 0, 5, 5, 0, 0,
   /* 0xa0 */
   2, 6, 2, 0, 3, 3, 3, 3,
   2, 2, 2, 2, 4, 4, 4, 4,
   /* 0xb0 */
-  2, 5, 0, 0, 4, 4, 4, 0,
+  2, 5, 0, 0, 4, 4, 4, 4,
   2, 4, 2, 0, 4, 4, 4, 0,
   /* 0xc0 */
-  2, 6, 2, 0, 3, 3, 5, 0,
+  2, 6, 2, 0, 3, 3, 5, 5,
   2, 2, 2, 2, 4, 4, 6, 0,
   /* 0xd0 */
-  2, 5, 0, 8, 4, 4, 6, 0,
+  2, 5, 0, 8, 4, 4, 6, 6,
   2, 4, 2, 0, 4, 4, 7, 0,
   /* 0xe0 */
-  2, 6, 2, 0, 3, 3, 5, 0,
+  2, 6, 2, 0, 3, 3, 5, 5,
   2, 2, 2, 2, 4, 4, 6, 0,
   /* 0xf0 */
-  2, 5, 1, 0, 4, 4, 6, 0,
+  2, 5, 1, 0, 4, 4, 6, 6,
   2, 4, 2, 0, 4, 4, 7, 0,
 };
 
@@ -446,8 +446,14 @@ defs_6502_init() {
   defs_65c12_set_opcode(0xE2, k_nop, k_imm); /* Undocumented. */
   defs_65c12_set_opcode(0xF2, k_sbc, k_id);
   defs_65c12_set_opcode(0xFA, k_plx, k_nil);
-  /* TODO: remaining 1-byte NOPs. */
+  /* 1-byte NOPs. */
   for (i = 0; i < 16; ++i) {
     defs_65c12_set_opcode(((i * 0x10) + 0x03), k_nop, k_nil1);
+    defs_65c12_set_opcode(((i * 0x10) + 0x07), k_nop, k_nil1);
+    /* 0xEB is SBC imm. */
+    if (i != 0xE) {
+      defs_65c12_set_opcode(((i * 0x10) + 0x0B), k_nop, k_nil1);
+    }
+    defs_65c12_set_opcode(((i * 0x10) + 0x0F), k_nop, k_nil1);
   }
 }

defs_6502.h
@@ -12,7 +12,7 @@ enum {
 };
 
 enum {
-  k_6502_op_num_types = 79,
+  k_6502_op_num_types = 80,
   k_6502_op_num_opcodes = 256,
   k_6502_op_num_modes = 17,
 };
@@ -100,14 +100,15 @@ enum {
   k_xaa = 68, /* Undocumented. */
   k_rra = 69, /* Undocumented. */
   k_axs = 70, /* Undocumented. */
-  k_tsb = 71, /* 65c12. */
-  k_trb = 72, /* 65c12. */
-  k_stz = 73, /* 65c12. */
-  k_bra = 74, /* 65c12. */
-  k_phx = 75, /* 65c12. */
-  k_phy = 76, /* 65c12. */
-  k_plx = 77, /* 65c12. */
-  k_ply = 78, /* 65c12. */
+  k_isc = 71, /* Undocumented. */
+  k_tsb = 72, /* 65c12. */
+  k_trb = 73, /* 65c12. */
+  k_stz = 74, /* 65c12. */
+  k_bra = 75, /* 65c12. */
+  k_phx = 76, /* 65c12. */
+  k_phy = 77, /* 65c12. */
+  k_plx = 78, /* 65c12. */
+  k_ply = 79, /* 65c12. */
 };
 
 enum {

commit de7e3d54518873a420dbb9907315b6a7adf18e58

Fix terrible timing error with mode zpx RMW.
The interpreter has it taking 5 cycles and it should be 6.
Causing Exile captures to not replay correctly across the different
modes.

Regressed here:
https://github.com/scarybeasts/beebjit/commit/7faa004dc219539b714d56f2c1f4b7e496c755a3
Regressed in v0.9.4(!)

interp.c
@@ -757,7 +757,7 @@ interp_check_log_bcd(struct interp_struct* p_interp) {
   v = p_mem_read[addr];                                                       \
   INSTR;                                                                      \
   p_mem_write[addr] = v;                                                      \
-  cycles_this_instruction = 5;
+  cycles_this_instruction = 6;
 
 #define INTERP_LOAD_NZ_FLAGS(reg_name)                                        \
   nf = !!(reg_name & 0x80);                                                   \

make_timing_rom.c
@@ -876,8 +876,16 @@ main(int argc, const char* argv[]) {
   emit_REQUIRE_EQ(p_buf, 18);
   emit_JMP(p_buf, k_abs, 0xCC80);
 
-  /* Exit sequence. */
   set_new_index(p_buf, 0x0C80);
+  emit_LDX(p_buf, k_imm, 0);
+  emit_CYCLES_RESET(p_buf);
+  emit_DEC(p_buf, k_zpx, 0xF0);     /* Mode zpx, RMW, 6 cycles. */
+  emit_CYCLES(p_buf);
+  emit_REQUIRE_EQ(p_buf, 14);
+  emit_JMP(p_buf, k_abs, 0xCCC0);
+
+  /* Exit sequence. */
+  set_new_index(p_buf, 0x0CC0);
   emit_EXIT(p_buf);
 
   /* Some program code that we copy to ROM at $E000 to RAM at $3000 */