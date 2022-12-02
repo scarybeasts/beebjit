scarybeasts/beebjit

commit 1565081621bc49db857390eb04a52be815d66add

Remove video:no-vsync-wait-for-render and infer it instead.
Now, slow mode waits for the render buffer to be pasted to the
display window, and fast mode does not.

bbc.c
@@ -124,7 +124,6 @@ struct bbc_struct {
   int fast_flag;
   int test_map_flag;
   int autoboot_flag;
-  int vsync_wait_for_render;
   int do_video_memory_sync;
   struct bbc_options options;
 
@@ -1168,13 +1167,15 @@ bbc_framebuffer_ready_callback(void* p,
   struct bbc_message message;
 
   struct bbc_struct* p_bbc = (struct bbc_struct*) p;
+  int do_wait_for_render = !p_bbc->fast_flag;
 
   message.data[0] = k_message_vsync;
   message.data[1] = do_full_render;
   message.data[2] = framing_changed;
   message.data[3] = timing_get_total_timer_ticks(p_bbc->p_timing);
+  message.data[4] = do_wait_for_render;
   bbc_cpu_send_message(p_bbc, &message);
-  if (bbc_get_vsync_wait_for_render(p_bbc)) {
+  if (do_wait_for_render) {
     struct bbc_message message;
     bbc_cpu_receive_message(p_bbc, &message);
     assert(message.data[0] == k_message_render_done);
@@ -1380,7 +1381,6 @@ bbc_create(int mode,
   p_bbc->test_map_flag = test_map_flag;
   p_bbc->is_wd_fdc = (wd_1770_type > 0);
   p_bbc->is_wd_1772 = (wd_1770_type == 2);
-  p_bbc->vsync_wait_for_render = 1;
   p_bbc->exit_value = 0;
   p_bbc->handle_channel_read_bbc = -1;
   p_bbc->handle_channel_write_bbc = -1;
@@ -1388,9 +1388,6 @@ bbc_create(int mode,
   p_bbc->handle_channel_write_client = -1;
   p_bbc->timer_id_autoboot = -1;
 
-  if (util_has_option(p_opt_flags, "video:no-vsync-wait-for-render")) {
-    p_bbc->vsync_wait_for_render = 0;
-  }
   p_bbc->do_video_memory_sync = 1;
   if (util_has_option(p_opt_flags, "video:no-memory-sync")) {
     p_bbc->do_video_memory_sync = 0;
@@ -2055,11 +2052,6 @@ bbc_get_run_flag(struct bbc_struct* p_bbc) {
 int
 bbc_get_print_flag(struct bbc_struct* p_bbc) {
   return p_bbc->print_flag;
-}
-
-int
-bbc_get_vsync_wait_for_render(struct bbc_struct* p_bbc) {
-  return p_bbc->vsync_wait_for_render;
 }
 
 static void

EXAMPLES
@@ -95,12 +95,12 @@ a particularly accurate emulation but for highly compatible beeb code which
 makes few timing assumptions, it works fine. For example, BASIC might be running
 on a 2MHz 6502, a 2MHz 65c12, a 3MHz 6502 co-processor, etc., so it is not
 picky.
-./beebjit -fast -opt sound:off,bbc:cycles-per-run=10000000,video:no-vsync-wait-for-render,video:frames-skip=5 -0 test/perf/clocksp.ssd
+./beebjit -fast -opt sound:off,bbc:cycles-per-run=10000000,video:frames-skip=5 -0 test/perf/clocksp.ssd
 And then in the emulator:
 CH."CLOCKSP"
 
 Or you can combine this with the headless mode above:
-echo 'CH."CLOCKSP"' | ./beebjit -fast -opt sound:off,bbc:cycles-per-run=10000000,video:no-vsync-wait-for-render,video:frames-skip=5 -0 test/perf/clocksp.ssd -terminal -headless
+echo 'CH."CLOCKSP"' | ./beebjit -fast -opt sound:off,bbc:cycles-per-run=10000000 -0 test/perf/clocksp.ssd -terminal -headless
 
 Overall CLOCKSP is about 7GHz on my old slow laptop. I've seen individual tests
 hit 10GHz but that was before the Intel meltdown / spectre fiasco and
@@ -113,7 +113,7 @@ highest level (which, contrary to what its instructions say, is Level 6,
 not Level 9, see http://ssb22.user.srcf.net/bchess.html ) without having to
 wait hours for each of the computer's moves:
 
-./beebjit -fast -opt bbc:cycles-per-run=10000000,video:no-vsync-wait-for-render,video:frames-skip=5 -0 ChessMicroPower.ssd
+./beebjit -fast -opt bbc:cycles-per-run=10000000,video:frames-skip=5 -0 ChessMicroPower.ssd
 
 
 9) Capture and replay.

TODO
@@ -40,7 +40,7 @@ improvements out one by one on a rainy day.
 These speeds are for JIT mode. In some cases here, interp mode is faster
 because of some very severe JIT interaction.
 
-(all: -mode jit -opt sound:off,video:no-vsync-wait-for-render)
+(all: -mode jit -opt sound:off)
 (5th gen i5, v0.9.4 or so)
 - Elite.ssd. Slams hardware registers in a tight loop reading keyboard.
 - Tarzan.ssd. Very slow (120MHz). interp is faster!! Looks to be a tight

bbc.h
@@ -126,7 +126,6 @@ void bbc_get_address_details(struct bbc_struct* p_bbc,
 
 int bbc_get_run_flag(struct bbc_struct* p_bbc);
 int bbc_get_print_flag(struct bbc_struct* p_bbc);
-int bbc_get_vsync_wait_for_render(struct bbc_struct* p_bbc);
 
 void bbc_set_channel_handles(struct bbc_struct* p_bbc,
                              intptr_t handle_channel_read_bbc,
@@ -135,7 +134,7 @@ void bbc_set_channel_handles(struct bbc_struct* p_bbc,
                              intptr_t handle_channel_write_client);
 
 struct bbc_message {
-  uint64_t data[4];
+  uint64_t data[5];
 };
 void bbc_client_send_message(struct bbc_struct* p_bbc,
                              struct bbc_message* p_message);

main.c
@@ -640,6 +640,7 @@ beebjit_main(void) {
       int do_clear_after_paint;
       int save_frame;
       uint64_t cycles;
+      int do_ack_rendered;
 
       bbc_client_receive_message(p_bbc, &message);
       if (message.data[0] == k_message_exited) {
@@ -650,6 +651,8 @@ beebjit_main(void) {
       do_full_render = message.data[1];
       do_clear_after_paint = message.data[2];
       cycles = message.data[3];
+      do_ack_rendered = message.data[4];
+
       save_frame = 0;
       if ((frame_cycles > 0) &&
           (cycles >= frame_cycles) &&
@@ -675,7 +678,7 @@ beebjit_main(void) {
           render_clear_buffer(p_render);
         }
       }
-      if (bbc_get_vsync_wait_for_render(p_bbc)) {
+      if (do_ack_rendered) {
         message.data[0] = k_message_render_done;
         bbc_client_send_message(p_bbc, &message);
       }

commit 7a0d5c235e83194925226b418fcbe69b14abcfe1

Fix issue with timing of recording screen output
This fixes a problem introduced by
1565081621bc49db857390eb04a52be815d66add.

bbc.c
@@ -1171,7 +1171,8 @@ bbc_framebuffer_ready_callback(void* p,
   struct bbc_message message;
 
   struct bbc_struct* p_bbc = (struct bbc_struct*) p;
-  int do_wait_for_render = !p_bbc->fast_flag;
+  int do_wait_for_render = !p_bbc->fast_flag ||
+                           video_has_paint_timer_triggered(p_bbc->p_video);
 
   message.data[0] = k_message_vsync;
   message.data[1] = do_full_render;

video.c
@@ -2185,4 +2185,9 @@ video_get_crtc_state(struct video_struct* p_video,
   *p_is_in_dummy_raster = p_video->in_dummy_raster;
 }
 
+int
+video_has_paint_timer_triggered(struct video_struct* p_video) {
+  return p_video->has_paint_timer_triggered;
+}
+
 #include "test-video.c"

video.h
@@ -74,4 +74,6 @@ void video_get_crtc_state(struct video_struct* p_video,
                           int* p_is_in_vert_adjust,
                           int* p_is_in_dummy_raster);
 
+int video_has_paint_timer_triggered(struct video_struct* p_video);
+
 #endif /* BEEBJIT_VIDEO_H */